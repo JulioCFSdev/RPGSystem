054681f87afd1f25ee61455a5d322356fe94a976
Julio Cesar <jcfds.snf21@uea.edu.br>
2023-04-05 04:31:05 -0300

    Adding Fuction to Return Mouse Position in Floor

--- Assets/Scripts/Gameplay/Mouse/MouseWorld.cs
+++ Assets/Scripts/Gameplay/Mouse/MouseWorld.cs
@@ -1,19 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 namespace Gameplay.Mouse
 {
     public class MouseWorld : MonoBehaviour
     {
-        // Update is called once per frame
-        void Update()
+        [SerializeField] private LayerMask mousePlaneLayerMask;
+
+        private static MouseWorld _instance;
+
+        private void Awake()
         {
-            if (Camera.main != null)
+            _instance = this;
+        }
+        
+        // Return Mouse Position in Floor
+        public static Vector3 GetRaycastPoint()
+        {
+            try
             {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                // Check if the mouse position on the mainCamera is colliding with any object. => return bool
-                print(Physics.Raycast(ray));
+                Camera mainCamera = Camera.main;
+                // Check if the "mainCamera" is present in the scene.
+                if (mainCamera == null)
+                {
+                    throw new NullReferenceException("Camera.main is null");
+                }
+        
+                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                print("Mouse Position in World is enable ? : " + Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, _instance.mousePlaneLayerMask));
+                return raycastHit.point;
+            }
+            catch (NullReferenceException e)
+            {
+                Debug.LogError(e);
+                throw;
             }
         }
     }    
