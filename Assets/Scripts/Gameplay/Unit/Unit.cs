9b07324fa0b6b352ddf0be35b6a520230086d13c
Julio Cesar <jcfds.snf21@uea.edu.br>
2023-04-05 06:15:51 -0300

    Adding Smooth rotation of the Unit

--- Assets/Scripts/Gameplay/Unit/Unit.cs
+++ Assets/Scripts/Gameplay/Unit/Unit.cs
@@ -20,16 +20,23 @@ namespace Gameplay.Unit
 
         void Update()
         {
-            float stoppingDistance = 0.1f;
             // Acceptance Margin for the new distance that the unit
+            float stoppingDistance = 0.1f;
             if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
             {
                 Vector3 moveDirection = (targetPosition - transform.position).normalized;
-                gameObject.transform.position += moveDirection * (Time.deltaTime * moveSpeed); 
+                gameObject.transform.position += moveDirection * (Time.deltaTime * moveSpeed);
+                
+                // Smooth rotation of the character towards the new selected position.
+                float rotateSpeed = 10f;
+                transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+                
+                // Assigning the value of "true" to the running animation parameter.
                 _unitAnimator.SetBool(_isWalkingAnimationParameter,true);
             }
             else
             {
+                // Assigning the value of "false" to the running animation parameter.
                 _unitAnimator.SetBool(_isWalkingAnimationParameter,false);
             }
             
